ff2503d1cf1d544683721ad2ca58c06692f47ba8
Ludo-q <l.qerraxhija@gmail.com>
2022-11-24 12:08:54 +0100

    Lecture 381. Placeholders and parameter substations

--- checkdb.py
+++ checkdb.py
@@ -2,7 +2,13 @@ import sqlite3
 
 conn = sqlite3.connect('contacts.sqlite')
 
-for name, phone, email in conn.execute('SELECT * FROM contacts'):
+user_input = input('Please enter a name to find: ')
+
+query = f'SELECT * FROM contacts WHERE name = ?'
+
+# If we need to pass a tuple with one value then we need to add a comma
+#  after the value, variable, etc.
+for name, phone, email in conn.execute(query, (user_input, )):
     print(f'name: {name}, phone: {phone}, email: {email}')
 
 conn.close()
